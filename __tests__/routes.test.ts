1e1bcce9c4edec9e478616d6f667cee117e6ce4f
M0jjmd <moamine517@gmail.com>
2024-09-13 09:59:18 +0200

    Change on description test

--- __tests__/routes.test.ts
+++ __tests__/routes.test.ts
@@ -2,12 +2,12 @@ import request from 'supertest'
 import app from '../app'
 
 describe('Rooms routes', () => {
-    it('should return 401 if no token is provided when accessing protected rooms route', async () => {
+    it('return 401 if accessing protected rooms route with no token', async () => {
         const response = await request(app).get('/rooms')
         expect(response.status).toBe(401)
     })
 
-    it('should return 200 if valid token is provided when accessing rooms route', async () => {
+    it('return 200 if valid token is provided when accessing rooms route', async () => {
         const validToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNzI2MjEzNzQyLCJleHAiOjE3MjYyMTczNDJ9.Sj2SFskB8XxA5krGuZT1sNO8QOQSSfEWIicRONWysUU'
         const response = await request(app)
             .get('/rooms')
