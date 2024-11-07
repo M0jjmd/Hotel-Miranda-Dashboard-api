d18f9fc7b55ac801e2b3ef2ffdfd1e50b7b97ddc
M0jjmd <moamine517@gmail.com>
2024-11-07 17:47:58 +0100

    Added moduleNameMapper to find app url

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,9 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/'],
-};
+    roots: ['<rootDir>/__tests__'],
+    moduleDirectories: ['node_modules', '<rootDir>'],
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+    },
+}  
